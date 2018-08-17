94eea9476b76e500543e17e7160c6b7298b9896e
DraggyVX <40361572+DraggyVX@users.noreply.github.com>
2018-08-17 14:19:03 -0600

    Add files via upload

---
+++ src/components/Market.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Market extends Component {
+    render() {
+        return (
+            <div> sample Text </div>
+        );
+    }
+}
+
+export default Market;
